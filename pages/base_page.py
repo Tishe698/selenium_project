cd45bd1d4f707107d8730b3041a47deb93b098d3
Tishe698 <Poke37454@yandex.ru>
2026-01-30 00:36:55 +0300

    delete import

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,6 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
 
 class BasePage:
